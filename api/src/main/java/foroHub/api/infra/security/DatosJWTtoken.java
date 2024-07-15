7d175b4a279224f38c4c36b3a48f685eb9e2d22a
Luis Vega <l2vega@live.com>
2024-07-14 23:20:29 -0600

    Final del camino

---
+++ api/src/main/java/foroHub/api/infra/security/DatosJWTtoken.java
@@ -0,0 +1,5 @@
+package foroHub.api.infra.security;
+
+public record DatosJWTtoken(String jwTtoken) {
+
+}
---
+++ api/src/main/java/foroHub/api/infra/security/TokenService.java
@@ -0,0 +1,48 @@
+package foroHub.api.infra.security;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.secret}")
+    private String apiSecret;
+
+    public String generarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("Foro_Hub")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException();
+        }
+    }
+
+    public String getSubject(String token) {
+        if (token == null) {
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
+            verifier = JWT.require(algorithm)
+                    .withIssuer("Foro_Hub")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
+    }
+
+    private Instant generarFechaExpiracion() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+    }
+
+}}
